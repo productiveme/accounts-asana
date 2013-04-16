dcebcf0b7191fa6cae77c59242611eeb9fca88d8
Jaco Swarts <jaco@productive.me>
2013-04-16 10:30:28 +0200

    Additional fixes - first round of testing

--- asana_server.js
+++ asana_server.js
@@ -1,55 +1,54 @@
-(function () {
-  Accounts.asana.setSecret = function (secret) {
-    Accounts.asana._secret = secret;
+Accounts.oauth.registerService('asana', 2, function(query) {
+
+  var response = getTokens(query);
+  var accessToken = response.accessToken;
+  var identity = response.identity;
+
+  var serviceData = {
+    accessToken: accessToken,
+    expiresAt: (+new Date) + (1000 * response.expiresIn),
+    id: response.identity.id
   };
 
-  Accounts.oauth.registerService('asana', 2, function(query) {
+  // only set the token in serviceData if it's there. this ensures
+  // that we don't lose old ones (since we only get this on the first
+  // log in attempt)
+  if (response.refreshToken)
+    serviceData.refreshToken = response.refreshToken;
 
-  	var tokenData = getAccessToken(query);
-    var accessToken = tokenData.access_token;
-    // var refreshToken = tokenData.refresh_token;
-    var identity = tokenData.user;
-    
-    return {
-      options: {
-		  services: {
-			  asana: {
-				  id: identity.id, 
-				  accessToken: accessToken,
-				  email: identity.email
-		  }}
-      },
-      extra: {profile: {name: identity.name}}
-    };
-  });
-
-  var getAccessToken = function (query) {
-	  var config = Accounts.loginServiceConfiguration.findOne({service: 'asana'});
-	  if (!config)
-		  throw new Accounts.ConfigError("Service not configured");
-	  
-	  var result = Meteor.http.post(
-			  "https://app.asana.com/-/oauth_token", {headers: {Accept: 'application/json'}, params: {
-				  code: query.code,
-				  client_id: config.clientId,
-				  client_secret: config.secret,
-				  redirect_uri: Meteor.absoluteUrl("_oauth/asana?close"),
-				  state: query.state
-			  }});
-	  if (result.error) // if the http response was an error
-		  throw result.error;
-	  if (result.data.error) // if the http response was a json object with an error attribute
-	      throw result.data;
-	  return result.data;
+  return {
+    serviceData: serviceData,
+    options: {profile: {name: identity.name}}
   };
+});
 
-  // var getIdentity = function (accessToken) {
-  //   var result = Meteor.http.get(
-  //     "https://api.github.com/user",
-  //     {params: {access_token: accessToken}});
+// returns an object containing:
+// - accessToken
+// - expiresIn: lifetime of token in seconds
+// - refreshToken, if this is the first authorization request
+var getTokens = function (query) {
+  var config = Accounts.loginServiceConfiguration.findOne({service: 'asana'});
+  if (!config)
+    throw new Accounts.ConfigError("Service not configured");
 
-  //   if (result.error)
-  //     throw result.error;
-  //   return result.data;
-  // };
-}) ();
+  var result = Meteor.http.post(
+    "https://app.asana.com/-/oauth_token", {params: {
+      code: query.code,
+      client_id: config.clientId,
+      client_secret: config.secret,
+      redirect_uri: Meteor.absoluteUrl("_oauth/asana?close"),
+      grant_type: 'authorization_code'
+    }});
+
+  if (result.error) // if the http response was an error
+    throw result.error;
+  if (result.data.error) // if the http response was a json object with an error attribute
+    throw result.data;
+
+  return {
+    accessToken: result.data.access_token,
+    refreshToken: result.data.refresh_token,
+    expiresIn: result.data.expires_in,
+    identity: result.data.data
+  };
+};
